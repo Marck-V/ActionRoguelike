b394e1355378d8a6b0551609105ced6d19a55d18
Marck-V <42965997+Marck-V@users.noreply.github.com>
2023-10-24 22:58:03 -0700

    AI working almost as intended

---
+++ Source/ActionRoguelike/Private/AI/SAIController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+void ASAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	RunBehaviorTree(BehaviorTree);
+	// Setting the MyPawn variable to the player pawn
+	APawn* MyPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+	// If MyPawn is not null then set the MoveToLocation to the player pawn's location.
+	if(MyPawn)
+	{
+		GetBlackboardComponent()->SetValueAsVector("MoveToLocation", MyPawn->GetActorLocation());
+
+		GetBlackboardComponent()->SetValueAsObject("TargetActor", MyPawn);
+	}
+}
